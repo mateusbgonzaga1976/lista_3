a049a083d80a2d9145b2bd9e2aa253b3a16b6713
mateusbgonzaga1976 <55139704+mateusbgonzaga1976@users.noreply.github.com>
2019-09-30 13:03:17 -0300

    Add files via upload
    lista pratica 3 exercicio 2

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(){
 
-int main()
-{
-    char frase[100];
-    int tamanho;
+    char str1[100];
+    printf("Digite um texto com no maximo 100 caracteres:\n");
+    gets(str1);
+    int vogal = 0;
 
-    printf ("Digite uma frase\n\n");
-    gets (frase);
+    for(int i = 0; i < sizeof(str1) && str1[i] != '\0'; i++){
+        if(str1[i] == 'a' || str1[i] == 'e' || str1[i] == 'i' || str1[i] == 'o' || str1[i] == 'u'){
+            vogal++;
+        }
+        if(str1[i] == 'A' || str1[i] == 'E' || str1[i] == 'I' || str1[i] == 'O' || str1[i] == 'U'){
+            vogal++;
+        }
+    }
 
-    tamanho = strlen(frase);
+    printf("Foram contadas um total de %d vogais no texto.\n", vogal);
 
-    printf("\nTotal de letras da Frase : %d",tamanho);
-    printf("\n\n");
-
-    system ("pause");
+    system("pause");
     return 0;
 }
+
+
